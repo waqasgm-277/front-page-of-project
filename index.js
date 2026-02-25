68283b03d9dd4891aa2b1ea4e4303247ccbc3bb7
waqasgm-277 <waqasgm277@gmail.com>
2026-02-25 16:17:10 +0500

    ye commit hogaya

---
+++ index.js
@@ -0,0 +1,41 @@
+let menu_list = document.getElementById("menu_list");
+menu_list.style.maxHeight = "0px"
+
+function toggleMenu(){
+
+    if(menu_list.style.maxHeight == "0px")
+    
+        menu_list.style.maxHeight = "300px"
+    
+    else{
+
+        menu_list.style.maxHeight = "0px"
+    }
+}
+
+
+
+const slides = document.querySelector('.slides');
+const images = document.querySelectorAll('.slides img');
+
+let index = 0;
+
+function showSlide() {
+  slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+document.querySelector('.next').addEventListener('click', () => {
+  index++;
+  if (index >= images.length) {
+    index = 0;
+  }
+  showSlide();
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+  index--;
+  if (index < 0) {
+    index = images.length - 1;
+  }
+  showSlide();
+});
