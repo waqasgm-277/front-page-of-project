3cce7d3897530e791152a15f8eabe2b54cb96d2a
waqasgm-277 <waqasgm277@gmail.com>
2026-02-28 22:48:40 +0500

    few changes here buddy

--- index.js
+++ index.js
@@ -80,9 +80,52 @@ document.querySelector('.prev').addEventListener('click', () => {
                       // slider ends here bruh
 
 
+      // json work here 
+
+ let html = "" 
+
+
+ fetch ('data.json')
+
+ .then (response => response.json())
+ .then(json => {
+
+    for (let val of json){
 
 
 
+      html +=    ` <div class="col-3">
+
+             <div class="card" style="width: 18rem;border: none;"  >
+
+               <img src="${p_image}" alt="..."  style="border-radius: 20px;">
+
+           <div class="card-body">
+
+         <h4 class="card-title" id="title" style="display: inline;">${val.p_title}</h4>
+
+       <div style="padding: 5px;font-size: 17px;" id="discription">${val.p_discription}</div>
+
+     <p id="rating" style="margin: 0;"><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i></p>
+     
+    <p style="font-size: 30px;font-family:'Times New Roman';display: inline;" id="price" >${val.p_price}</p>
+    
+    
+     <button class="view" style="height: 40px;width: 100%;border-radius: 40px; background:linear-gradient(135deg, #bf00e6, #f69, #ff9f1a);; outline: none; border:none; color: white;margin-top: 3px;">Register</button>
+  </div>
+</div>
+                </div>
+`
+
+
+
+    }
+
+
+
+
+
+ })
 
 
 
